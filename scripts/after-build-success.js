168e7f3760c7d2b88bf60c580e760c83729000b5
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-09-01 11:13:28 -0400

    Release packages from dist folder

--- scripts/after-build-success.js
+++ scripts/after-build-success.js
@@ -1,15 +1,36 @@
 const fs = require('fs-extra');
 const path = require('path');
 
+function copyFilesToDist() {
+  const pathsToCopy = [
+    ['README.md'],
+    ['CHANGELOG.md'],
+    ['package.json'],
+    ['builders.json'],
+    ['collection.json']
+  ];
+
+  pathsToCopy.forEach(pathArr => {
+    const sourcePath = path.join(...pathArr);
+    const distPath = path.join('dist', ...pathArr);
+    if (fs.existsSync(sourcePath)) {
+      fs.copySync(sourcePath, distPath);
+      console.log(`Successfully copied ${sourcePath} to ${distPath}`);
+    } else {
+      throw `File not found: ${sourcePath}`;
+    }
+  });
+}
+
 function mergeBuilderSchemas() {
   // const schemaConfigs = [
   //   {
   //     baseSchemaPath: './node_modules/@angular-devkit/build-angular/src/browser/schema.json',
-  //     schemaPath: './dist/src/builders/browser/schema.ext.json'
+  //     schemaPath: './src/browser/schema.ext.json'
   //   },
   //   {
   //     baseSchemaPath: './node_modules/@angular-devkit/build-angular/src/dev-server/schema.json',
-  //     schemaPath: './dist/src/builders/dev-server/schema.ext.json'
+  //     schemaPath: './src/dev-server/schema.ext.json'
   //   }
   // ];
 
@@ -20,11 +41,12 @@ function mergeBuilderSchemas() {
   //   const newJson = Object.assign({}, baseSchemaJson, schemaJson);
   //   newJson.properties = Object.assign({}, baseSchemaJson.properties, schemaJson.properties || {});
 
-  //   fs.writeJsonSync(config.schemaPath, newJson, {
+  //   fs.writeJsonSync(path.join('dist', config.schemaPath), newJson, {
   //     encoding: 'utf8',
   //     spaces: 2
   //   });
   // });
 }
 
+copyFilesToDist();
 mergeBuilderSchemas();
