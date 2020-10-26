19bf3ecc55d6c179c0b47e79929ef4d94c525550
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-10-26 17:08:34 -0400

    Re-add images

--- builders-test-app/src/app/lazy-loaded/lazy-loaded.component.ts
+++ builders-test-app/src/app/lazy-loaded/lazy-loaded.component.ts
@@ -6,6 +6,10 @@ import {
   selector: 'app-lazy-loaded',
   template: `
 <p>Lazy loaded!</p>
+<h3>Image 1, lazy</h3>
+<img src="assets/bb-image-1.jpg">
+<h3>Image 2, lazy</h3>
+<img src="assets/bb-image-2.png">
 `
 })
 export class LazyLoadedComponent { }
