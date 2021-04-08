00d143e50aacf92b0693dbb15b4124ae1ad4e881
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-04-08 13:23:20 -0400

    Add comment

--- src/builders/browser/browser-options.ts
+++ src/builders/browser/browser-options.ts
@@ -1,5 +1,8 @@
 import { BrowserBuilderOptions } from '@angular-devkit/build-angular';
 
 export type SkyuxBrowserBuilderOptions = BrowserBuilderOptions & {
+  /**
+   * Specifies if the build results should be served locally.
+   */
   skyuxServe?: boolean;
 };
