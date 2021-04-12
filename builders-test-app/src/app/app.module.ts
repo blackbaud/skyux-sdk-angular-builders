42b427f12e2e5dc5c23fe50a4cda996a06d06eb6
Paul Crowder <paul.crowder@blackbaud.com>
2021-04-12 16:23:03 -0400

    Init viewkeeper

--- builders-test-app/src/app/app.module.ts
+++ builders-test-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SkyViewkeeperModule } from '@skyux/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { SkyuxModule } from './__skyux/skyux.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SkyuxModule.forRoot()
+    SkyuxModule.forRoot(),
+    SkyViewkeeperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
