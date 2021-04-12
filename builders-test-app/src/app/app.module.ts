0daac6fdc3724fbc9d97c369def9c1a11ec493db
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-04-12 12:29:00 -0400

    Set ROOT_DIR in Karma builder

--- builders-test-app/src/app/app.module.ts
+++ builders-test-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SkyI18nModule } from '@skyux/i18n';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { SkyuxModule } from './__skyux/skyux.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SkyI18nModule,
     SkyuxModule.forRoot()
   ],
   providers: [],
